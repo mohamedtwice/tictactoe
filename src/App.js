84aed7420b1accbcc689599d4eefda1e377b0596
mohamedtwice <mohamedtwice@gmail.com>
2017-09-08 00:30:22 -0500

    win/draw logic complete

--- src/App.js
+++ src/App.js
@@ -6,60 +6,60 @@ class App extends Component {
 constructor() {
     super();
     this.state = {
+      winner: undefined,
+    };
+    this.gameState = {
         turn: 'X',
+        gameLocked: false,
         gameEnded: false,
-        winner: undefined,
-        board: Array(9).fill(''),
-        totalMoves: 0
-
+       board: Array(9).fill(''),
+       totalMoves: 0
     }
   }
 
 clicked(event) {
 
-if (this.state.board[event.target.dataset.square] == '') {
-this.state.board[event.target.dataset.square] = this.state.turn;
-  event.target.innerText = this.state.turn;
-  this.setState({
-    turn: this.state.turn == 'X' ? 'O' : 'X',
-    board: this.state.board,
-    totalMoves: this.state.totalMoves++
-  })
+    if(this.gameState.gameEnded) return;
+
+if (this.gameState.board[event.target.dataset.square] == '') {
+this.gameState.board[event.target.dataset.square] = this.gameState.turn;
+  event.target.innerText = this.gameState.turn;
+  this.gameState.turn = this.gameState.turn == 'X' ? 'O' : 'X',
+  this.gameState.totalMoves++
   }
 
 var result = this.checkWinner();
 
     if(result == 'X') {
+      this.gameState.gameEnded = true;
       this.setState({
-        gameEnded: true,
         winner: 'X',
-        winnerLine: 'Match won by X'
+        winnerLine: 'Match won by X!'
       });
         console.log('X Wins!')
     } else if(result == 'O') {
+      this.gameState.gameEnded = true;
       this.setState({
-        gameEnded: true,
         winner: 'O',
-        winnerLine: 'Match won by O'
+        winnerLine: 'Match won by O!'
       });
         console.log('O Wins!')
     } else if(result == 'draw') {
       this.gameState.gameEnded = true;
       this.setState({
-        gameEnded: true,
         winner: 'draw',
-        winnerLine: 'Match is drawn'
-      });
+        winnerLine: 'Match is draw!'
+      })
     console.log('Draw!')
 
     }
 
-  console.log(this.state.board)
+  console.log(this.gameState.board)
 }
 
 checkWinner() {
     var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]];
-    var board = this.state.board;
+    var board = this.gameState.board;
 
     for(let i=0;i<moves.length;i++) {
       if(board[moves[i][0]] == board[moves[i][1]] && board[moves[i][1]] == board[moves[i][2]])
