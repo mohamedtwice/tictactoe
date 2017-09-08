c5079c9ecb03f3edef627de7e4142edf9c7cf0cb
mohamedtwice <mohamedtwice@gmail.com>
2017-09-08 00:06:48 -0500

    more functionality

--- src/App.js
+++ src/App.js
@@ -7,17 +7,72 @@ constructor() {
     super();
     this.state = {
         turn: 'X',
-        gameEnded: false
+        gameEnded: false,
+        winner: undefined,
+        board: Array(9).fill(''),
+        totalMoves: 0
+
     }
   }
 
 clicked(event) {
-  console.log(event.target)
+
+if (this.state.board[event.target.dataset.square] == '') {
+this.state.board[event.target.dataset.square] = this.state.turn;
   event.target.innerText = this.state.turn;
   this.setState({
-    turn: this.state.turn == 'X' ? 'O' : 'X'
+    turn: this.state.turn == 'X' ? 'O' : 'X',
+    board: this.state.board,
+    totalMoves: this.state.totalMoves++
   })
+  }
+
+var result = this.checkWinner();
+
+    if(result == 'X') {
+      this.setState({
+        gameEnded: true,
+        winner: 'X',
+        winnerLine: 'Match won by X'
+      });
+        console.log('X Wins!')
+    } else if(result == 'O') {
+      this.setState({
+        gameEnded: true,
+        winner: 'O',
+        winnerLine: 'Match won by O'
+      });
+        console.log('O Wins!')
+    } else if(result == 'draw') {
+      this.gameState.gameEnded = true;
+      this.setState({
+        gameEnded: true,
+        winner: 'draw',
+        winnerLine: 'Match is drawn'
+      });
+    console.log('Draw!')
+
+    }
+
+  console.log(this.state.board)
 }
+
+checkWinner() {
+    var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    var board = this.state.board;
+
+    for(let i=0;i<moves.length;i++) {
+      if(board[moves[i][0]] == board[moves[i][1]] && board[moves[i][1]] == board[moves[i][2]])
+          return board[moves[i][0]];
+    }
+
+    console.log(this.gameState.totalMoves);
+    if(this.gameState.totalMoves == 9) {
+      return 'draw';
+    }
+
+  }
+
 
   render() {
     return (
@@ -27,7 +82,8 @@ clicked(event) {
         </div>
         <div id="game">
           <div id="head">
-              
+                     <div id="status">{this.state.winnerLine}</div>
+   
           </div>
           <div id="board" onClick={(e)=>this.clicked(e)}>
               <div className="square" data-square="0"></div>
